ffd47f79bf925bb8c1d0ea2a855567becdf1f29a
Tom Schons <schons.tom@gmail.com>
2015-07-05 20:52:26 +0200

    bux fixes

--- app/src/main/java/tud/tk3/splitris/Tetris/AppPreferences.java
+++ app/src/main/java/tud/tk3/splitris/Tetris/AppPreferences.java
@@ -3,6 +3,8 @@ package tud.tk3.splitris.Tetris;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+import java.util.List;
+
 import tud.tk3.splitris.R;
 
 public class AppPreferences extends PreferenceActivity {
@@ -11,7 +13,13 @@ public class AppPreferences extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        // addPreferencesFromResource(R.xml.preferences);
+    }
+
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences, target);
     }
 
 }
