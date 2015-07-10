444d0d3ccf2a758ac77f1c35651e7b968630b709
Fee0 <frederik.peukert@gmx.de>
2015-07-10 11:55:40 +0200

    image continued

--- app/src/main/java/tud/tk3/splitris/ImageActivity.java
+++ app/src/main/java/tud/tk3/splitris/ImageActivity.java
@@ -1,15 +1,34 @@
 package tud.tk3.splitris;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import tud.tk3.splitris.network.GameController;
+import tud.tk3.splitris.network.Player;
+import tud.tk3.splitris.tetris.Initiator;
+import tud.tk3.splitscreen.output.ScreenView;
+import tud.tk3.splitscreen.output.Viewport;
+import tud.tk3.splitscreen.screen.BlockScreen;
 
 public class ImageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //FIXME
         setContentView(R.layout.gameactivity);
 
+        ScreenView image_screen = (ScreenView) findViewById(R.id.image_screen);
+
+        //We are at the Client
+        if(ImageContext.Client != null) {
+            ImageContext.Client.registerView(0, image_screen);
+        }
+        //We are at the Server
+        else {
+            Initiator init = new Initiator();
+            BlockScreen bs = init.configureBlockScreens(ImageContext.Contributers, image_screen);
+            ImageContext.startDisplay(bs);
+        }
     }
 }
--- app/src/main/java/tud/tk3/splitris/ImageContext.java
+++ app/src/main/java/tud/tk3/splitris/ImageContext.java
@@ -1,13 +1,44 @@
 package tud.tk3.splitris;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import tud.tk3.splitris.image.Image;
 import tud.tk3.splitris.network.GameServer;
+import tud.tk3.splitris.network.Player;
+import tud.tk3.splitscreen.network.Client;
 import tud.tk3.splitscreen.screen.BlockScreen;
 
 public class ImageContext {
     public static Image Image;
+    public static int PORT = 45832;
+    public static GameServer Server;
+    public static tud.tk3.splitscreen.network.Client Client;
+    public static ArrayList<Player> Contributers;
 
     public static void startDisplay(BlockScreen bs) {
         Image = new Image(bs);
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                try {
+
+                    //TODO
+
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }.start();
+    }
+
+    public static void initClient() {
+        Client = new Client();
+    }
+
+    public static void initServer(String nickname) throws IOException {
+        Server = new GameServer(PORT, nickname);
+        Server.start();
     }
 }
